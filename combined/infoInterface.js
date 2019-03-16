b2a652214525bbfa62a597ec1f67fcf9add62808
yhj <jlmctsb@gmail.com>
2019-03-16 11:59:29 +0800

    信息+详细信息

--- combined/infoInterface.js
+++ combined/infoInterface.js
@@ -1,4 +1,5 @@
-$.ajax({
+var postId;
+window.onload=function(){$.ajax({
     url:"../deal.php",
     type:"POST",
     dataType:"JSON",
@@ -9,8 +10,8 @@ $.ajax({
 success:function(data){
     var info=data.infos;
     for(var i=0;i<info.length;i++){
-    var adding=$('<div class="list-group"></div>');
-    adding.innerHTML="<a class=\"list-group-item active\" id=info[i].id>"+
+    var adding=$('<div class="list-group" id=info[i].id></div>');
+    adding.innerHTML="<a class=\"list-group-item active\" >"+
         "<h4 class=\"list-group-item-heading\">"+
             info[i].title+" &yen;"+info[i].gift+"<span style=\"float:right\">"+info[i].type+"</span>"
             "</h4><span class=\"list-group-item\">"+
@@ -24,11 +25,47 @@ success:function(data){
 }},
 error:function(Error){
     alert(Error);
-}});
+}});}
+
+function confirmBillBtn() {
+    var result = confirm("确认");
+    if (result == true) {
+        $.ajax({
+            url:"../deal.php",
+            type:"POST",
+            dataType:"JSON",
+            data:{
+                "op":"acceptInfo",
+                "id":postId
+            },
+            success:function(data){
+                if(data==0){
+                    alert("接单成功\n请按时完成任务哦");
+                }
+                else if(data==2){
+                    alert("请登录后接单!");
+
+
+
+                    /*这是登录界面*/
+                }
+                else if(data==3){
+                    alert("接取失败,错误未知");
+                }
+            }
+        });
+    } else {
+    }
+}
+
+function cancelBillBtn(){
+    $("#detailed").slideUp();
+}
 
 $(document).ready(function(){
-    $("a").click(function(){
-        var postId=$("this").attr("id");
+    $('div.list-group').on('click','a',function(){
+        postId=$(this).parent().attr("id");
+        var s = $(this).parent();
         $.ajax({
             url:"../deal.php",
             type:"POST",
@@ -38,6 +75,22 @@ $(document).ready(function(){
                 "id":postId
             },
             success:function(data){
+                var detailedInfo=data.info;
+                var infoHtml='<div id="detailed" class="alert alert-success">'+
+                "<a class=\"list-group-item active\" >"+
+        "<h4 class=\"list-group-item-heading\">"+
+            detailedInfo.title+" &yen;"+detailedInfo.gift+'<span style="color:#808080;font-size:40px;">'+'&nbsp;&nbsp;'+detailedInfo.time+'</span>'+"<span style=\"float:right\">"+detailedInfo.type+"</span>"
+            "</h4><span class=\"list-group-item\">"+
+                "<h4 class=\"list-group-item-heading\">"+detailedInfo.name+"<span style=\"float:right\">发布人评分:"+detailedInfo.score+"</span></h4>"+
+                +'<p style="float:left" class="list-group-item-text">detailedInfo.content</p>'+"<p class=\"list-group-item-text\">"+
+                    "<div style=\"float:right\">预计耗时:"+detailedInfo.lasting+"</div><br>"+'<div style="float:left">预计花费金额:'+detailedInfo.money+'</div>'
+                    +"送达截止时间:"+detailedInfo.deadline+"<br></p></span></a></div>"+
+                    '<div class="btn-group" style="float:right">'+
+       '<button type="button" id="cancel" class="btn btn-default" onclick="cancelBillBtn()">取消</button>'+
+        '<button class="btn btn-primary" type="button" onclick="confirmBillBtn()">确认</button>'+
+'</div>';
+            s.append(infoHtml);
+            
 
             }
 
