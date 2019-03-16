06a53f572ca15c9e838bf5ffc06af34d13f8128c
yhj <jlmctsb@gmail.com>
2019-03-16 14:29:09 +0800

    合并了js文件

--- combined/orderpage.js
+++ combined/orderpage.js
@@ -1,24 +1,3 @@
-// 这里是控制选项卡点击事项的
-window.onload = function(){
-    var oTab=document.getElementById("selectionTab");
-    var aH3=oTab.getElementsByTagName("h3");
-    var oPage=document.getElementById("inPage");
-    var aDiv=oPage.getElementsByClassName("oPages");
-    for (var i=0;i<aH3.length;i++){
-        aH3[i].index=i;
-        aH3[i].onclick=function () {
-            for (var j=0;j<aH3.length;j++){
-                aH3[j].className="";
-                aDiv[j].style.display = "none";
-                aDiv[this.index].className = "oPages";
-                aDiv[this.index].className = "content oPages";
-            }
-            this.className="active";
-            aDiv[this.index].style.display="block";
-        }
-    }
-}
-
 function confirmBtn() {
     var result = confirm("确认要完成订单吗？");
     if (result == true) {
