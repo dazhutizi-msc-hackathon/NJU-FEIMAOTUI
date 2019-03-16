4a9158a000e24972ae51166bfa36960237329abe
yhj <jlmctsb@gmail.com>
2019-03-16 14:56:44 +0800

    上次未做的更改

--- combined/infoInterface.js
+++ combined/infoInterface.js
@@ -1,28 +1,31 @@
 var postId;
-function writeInfo(){
-    $.ajax({
-        url:"deal.php",
-        type:"POST",
-        dataType:"JSON",
-        data:{"op":"getInfoList" },
-        success:function(data){
-            var info=data.infos;
-            for(var i=0;i<info.length;i++){
-            var adding=$('<div class="list-group" id=info[i].id></div>');
-            adding.innerHTML="<a class=\"list-group-item active\" >"+
-                "<h4 class=\"list-group-item-heading\">"+
-                    info[i].title+" &yen;"+info[i].gift+"<span style=\"float:right\">"+info[i].type+"</span>"
-                    "</h4><span class=\"list-group-item\">"+
-                        "<h4 class=\"list-group-item-heading\">"+info[i].name+"<span style=\"float:right\">发布人评分:"+info[i].score+"</span></h4>"+
-                        "<p class=\"list-group-item-text\">"+
-                            "<div style=\"float:right\">预计耗时:"+info[i].lasting+"</div><br>"+
-                            "送达截止时间:"+info[i].deadline+"<br><br></p></span></a>";
-            document.write(adding);
-        }},
-        error:function(Error){
-            alert(Error);
-        }});
-};
+function writeInfo(){$.ajax({
+    url:"../deal.php",
+    type:"POST",
+    dataType:"JSON",
+    data:{
+       "op":"getInfoList" 
+    }
+,
+success:function(data){
+    var info=data.infos;
+    for(var i=0;i<info.length;i++){
+    var adding=$('<div class="list-group" id=info[i].id></div>');
+    adding.innerHTML="<a class=\"list-group-item active\" >"+
+        "<h4 class=\"list-group-item-heading\">"+
+            info[i].title+" &yen;"+info[i].gift+"<span style=\"float:right\">"+info[i].type+"</span>"
+            "</h4><span class=\"list-group-item\">"+
+                "<h4 class=\"list-group-item-heading\">"+info[i].name+"<span style=\"float:right\">发布人评分:"+info[i].score+"</span></h4>"+
+                "<p class=\"list-group-item-text\">"+
+                    "<div style=\"float:right\">预计耗时:"+info[i].lasting+"</div><br>"+
+                    "送达截止时间:"+info[i].deadline+"<br><br></p></span></a>";
+    document.write(adding);
+
+
+}},
+error:function(Error){
+    alert(Error);
+}});}
 
 function confirmBillBtn() {
     var result = confirm("确认");
@@ -36,17 +39,17 @@ function confirmBillBtn() {
                 "id":postId
             },
             success:function(data){
-                if(data.code==0){
+                if(data==0){
                     alert("接单成功\n请按时完成任务哦");
                 }
-                else if(data.code==2){
+                else if(data==2){
                     alert("请登录后接单!");
 
 
 
                     /*这是登录界面*/
                 }
-                else if(data.code==3){
+                else if(data==3){
                     alert("接取失败,错误未知");
                 }
             }
